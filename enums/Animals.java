35b01863f1419d263dfd23e4fd9eacf7912632b0
Artem <84192047+frizyyu@users.noreply.github.com>
2023-12-11 21:14:09 +0300

    Add files via upload

---
+++ enums/Animals.java
@@ -0,0 +1,8 @@
+package enums;
+
+public enum Animals{
+    DINOSAURS,
+    COCKROACHES,
+    BUGS
+
+}
---
+++ enums/Things.java
@@ -0,0 +1,7 @@
+package enums;
+
+public enum Things {
+    SUITCASES,
+    KETTLEBELLS,
+    GARBAGE
+}
