4a9b3f843f74958f5a6020767625f2f5aab63307
Artem <84192047+frizyyu@users.noreply.github.com>
2023-12-13 14:22:54 +0300

    Delete Characters directory

--- Characters/Animal.java
+++
@@ -1,8 +0,0 @@
-package Characters;
-
-public class Animal extends Character.Animal{
-    @Override
-    public void TellYourRole(){
-        System.out.println("Животные отбежали в сторонку и стали поглядывать на незнайку своими глазками. С тревогой осмотревшись по сторонам.");
-    }
-}
